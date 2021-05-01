3e5952160a881785bac15f6850c0c007c806fb8c
LCypherus <75752265+LCypherus@users.noreply.github.com>
2021-05-01 22:56:46 +0200

    Update welcome.js
    Testing out a shorter welcoming message

--- commands/welcome.js
+++ commands/welcome.js
@@ -8,15 +8,7 @@ module.exports = (client) => {
     client.on('guildMemberAdd', (member) => {
         console.log(member);
 
-        const message = 
-`Welcome <@${member.id}> to Pen-and-Players Roleplay Server. We’re a ttrpg server open to all forms of ttrpg, whether that’s 5E, 4E, pathfinder, PTU, any and all are ttrpg systems welcome here in our community.
-                    
-We ask you please head over to ${member.guild.channels.cache.get(channelGetaRole).toString()} first to get yourself the proper tag you’d like to have for our server, and then hop into ${member.guild.channels.cache.get(channelGeneralTalk).toString()}  to chat with us!
-                    
-At ${member.guild.channels.cache.get(channelCurrentGames).toString()} you find all the tables and if they are in need of new players. Your can also check for available seats in a table to check the category name. If you wish to start your own table then head over to ${member.guild.channels.cache.get(channelTicketInfo).toString()}  to open a ticket so we can talk about it.
-                    
-Again, Welcome and enjoy your stay!
-<@453208397618085899> and <@283395860035928075>`;
+        const message = `Hi <@${member.id}> `;
 
         const channel = member.guild.channels.cache.get(channelId);
         channel.send(message);
