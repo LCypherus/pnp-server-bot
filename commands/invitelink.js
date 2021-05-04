ed27c6106868de208c106f01ab748f3ffb1e8528
LCypherus <km1609@gmail.com>
2021-05-04 16:37:19 +0200

    Changed a few stuff in invitelink.js

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -1,15 +1,29 @@
+const Discord = require('discord.js');
+
 module.exports = {
-    name: 'Invitelink',
-    description: "Embeds! Invitelink",
     execute(msg, args, Discord) {
-        const newEmbed = new Discord.MessageEmbed()
-        .setColor('#8B0000')
-        .setTitle('Invite link')
-        .setURL('https://discord.gg/ZN4DrdGg')
-        .setDescription('This is the invite link to invite players to this server.\n\nhttps://discord.gg/ZN4DrdGg\n\n Enjoy!')
-        .setFooter('If your having a problem with the invite link. Contact L_Cypherus.');
-
-        msg.channel.send(newEmbed);
+        msg.channel.send("", {
+            embed: {
+                "content": null,
+                "embeds": [
+                {
+                    "title": "Pen and Players Roleplayer Server Invite Link",
+                    "description": "This is the invite link to invite players to this server.",
+                    "color": 5814783,
+                    "fields": [
+                    {
+                        "value": "https://discord.gg/ZN4DrdGg"
+                    },
+                    {
+                        "value": "Enjoy!"
+                    }
+                    ],
+                    "footer": {
+                    "text": "If your having a problem with the invite link. Contact L_Cypherus."
+                    }
+                }
+                ]
+            }
+        });
     }
-
 }
