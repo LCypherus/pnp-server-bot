6801d6cea5347f8da59f9755ff1c89f3e9dfdeed
LCypherus <km1609@gmail.com>
2021-05-05 10:13:05 +0200

    Changing commands.js back

--- commands.js
+++ commands.js
@@ -8,6 +8,7 @@ module.exports = async function (msg) {
     let command = tokens.shift();
     if (command.charAt(0) === '&') {
         command = command.substring(1);
-        commands[command](msg, tokens);
+        var cmd = commands[command];
+        if (cmd && cmd.execute) cmd.execute(msg, tokens);
     }
 };
