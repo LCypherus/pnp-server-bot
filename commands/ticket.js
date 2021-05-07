7bea31b9dc52a38c0a5bd42f3d6e4c3a0fce85b8
LCypherus <km1609@gmail.com>
2021-05-07 22:01:24 +0200

    Added the &ticket newTable command

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,32 +1,29 @@
 const Discord = require("discord.js");
  
 module.exports = async function (client, msg, args){
-    /*
-    This first part here works.
-    */
     const guild = client.guilds.cache.get('685583244154109986');
     let ticketInfo = "This is a general ticket.";
     let ticketPrivate = false;
+    let ticketNewTable = false;
     if (args.length > 0){
         let start = 1;
         if (args[1] == "adminsOnly"){
             start = 2;
             ticketPrivate = true;
         }
+        else if (args[1] == "newTable"){
+            start = 2;
+            ticketNewTable = true;
+        }
         let ticketInfo = args.slice(start, args.length);
         ticketInfo = ticketInfo.join(" ");
-
-        console.log(msg.author)
     }
-    /* 
-    Create a new channel.
-    Needs a user ID. Not sure how to get this from Discord.
-    I'm guessing it's msg.author.id? Anyway, not sure if
-    anything below this point works. Needs testing.
-    */
-    const ticketCategory = "834881936454713424"; // Set this to the number for the category for Tickets
+
+    // Create a new channel.
+
+    const ticketCategory = "834881936454713424"; // category for Tickets
     if (ticketPrivate){
-        guild.channels.create("Ticket: " + msg.author.username,{ // msg.author might be incorrect
+        const createdChannel = await guild.channels.create("Ticket private: " + msg.author.username,{ 
         type: "text",
         parent: ticketCategory,
         permissionOverwrites: [
@@ -35,7 +32,7 @@ module.exports = async function (client, msg, args){
                 deny: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
             },
             {
-                id: msg.guild.roles.cache.get("733822632004288553"), // Grand Master, fill this out
+                id: msg.guild.roles.cache.get("733822632004288553"), // Grand Master
                 allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
             },
             {
@@ -44,9 +41,35 @@ module.exports = async function (client, msg, args){
             }
         ]
         })
+        createdChannel.send(ticketInfo);
+    }
+    else if (ticketNewTable){
+        const createdChannel = await guild.channels.create("Ticket new table: " + msg.author.username,{ 
+        type: "text",
+        parent: ticketCategory,
+        permissionOverwrites: [
+            {
+                id: msg.guild.roles.everyone, // Everyone
+                deny: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+            },
+            {
+                id: "733822632004288553", // Grand Master
+                allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+            },
+            {
+                id: "817145865105178624", // Assistant Grand Master
+                allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+            },
+            {
+                id: msg.author.id,
+                allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
+            }
+        ]
+        })
+        createdChannel.send(ticketInfo);
     }
     else{
-        guild.channels.create("Ticket for " + msg.author.username,{ // msg.author might be incorrect
+        const createdChannel = await guild.channels.create("Ticket for 2 " + msg.author.username,{ 
         type: "text",
         parent: ticketCategory,
         permissionOverwrites: [
@@ -55,11 +78,11 @@ module.exports = async function (client, msg, args){
                 deny: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
             },
             {
-                id: "733822632004288553", // Grand Master, fill this out
+                id: "733822632004288553", // Grand Master
                 allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
             },
             {
-                id: "817145865105178624", // Assistant Grand Master, fill this out
+                id: "817145865105178624", // Assistant Grand Master
                 allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"]
             },
             {
@@ -68,5 +91,8 @@ module.exports = async function (client, msg, args){
             }
         ]
         })
+        createdChannel.send(ticketInfo);
+
     }
+
 }
