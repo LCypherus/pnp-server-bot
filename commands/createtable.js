84125560a6fe7e969ac54e0d251a4c490f9f330b
LCypherus <km1609@gmail.com>
2021-05-10 21:42:20 +0200

    Update createtable.js

--- commands/createtable.js
+++ commands/createtable.js
@@ -4,14 +4,25 @@ const fetch = require('node-fetch');
 module.exports = async function(client, msg, args) {
     let tableName = 'dnd';
     let dm = msg.author.id;
-    let game = 822737501411737602; //dnd role
-    let role = 822737501411737602; //dnd role
-    let categoryId = 720689721168101435;
     if (args.length > 0) {
         dm = args[1];
         tableName = args.slice(2,args.length).join(" ");
         game = args[0];
     }
+    let spectator = "733945802296393793";
+    let rolesId = "840834624501448744"; // Testrole
+    let gameid = "822737501411737602"; // DnD Role
+
+    if (game == "dnd"){
+        gameid = "822737501411737602"; 
+    }
+    else if (game == "ptu"){
+        gameid = "832017680117923881";
+    }
+    else if (game == "epithet"){
+        gameid = "822737950025711636";
+    }
+    else {}
 
     let tableShorthand = tableName.match(/(?:^| )(\w)/g).join("").replace(/ /gi, ""); // This should be a string without spaces
 
@@ -25,6 +36,8 @@ module.exports = async function(client, msg, args) {
         msg.channel.send("Table Name: " + tableName);
         msg.channel.send("Table Shorthand: " + tableShorthand); // the shorthand identifier for each table; mz, anb, etc.
 
+        console.log(spectator, game, rolesId, dm)
+
         guild.roles.create({
             data: {
                 name: tableName,
@@ -34,19 +47,41 @@ module.exports = async function(client, msg, args) {
             rolesId = role.id;
             return msg.guild.channels.create(tableName, {
                 type: 'category',
-                permissionOverwrites: [{
-                    id: rolesId,
-                    allow: ['VIEW_CHANNEL'],
-                }]
+                permissionOverwrites: [
+                    {
+                        id: msg.guild.roles.everyone, // Everyone
+                        allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                    },
+                    {
+                        id: spectator, // Spectator
+                        allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                    },
+                    {
+                        id: gameid, // Game Format
+                        allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                    },
+                    {
+                        id: rolesId,
+                        allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS", "ADD_REACTIONS", "SEND_TTS_MESSAGES"],
+                    },
+                    {
+                        id: dm,
+                        allow: ["MANAGE_CHANNELS", "ADD_REACTIONS", "PRIORITY_SPEAKER", "STREAM", "VIEW_CHANNEL", "SEND_MESSAGES", "SEND_TTS_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES", "READ_MESSAGE_HISTORY", "MENTION_EVERYONE", "USE_EXTERNAL_EMOJIS", "CONNECT", "SPEAK", "MUTE_MEMBERS", "DEAFEN_MEMBERS", "USE_VAD", ]
+                    }
+                ]
             });
         }).then(cat => {
-            msg.guild.channels.create(tableShorthand, {
+            msg.guild.channels.create(tableShorthand + "info", {
                 type: 'text',
                 parent: cat,
                 permissionOverwrites: [{
                     id: rolesId,
                     allow: ['VIEW_CHANNEL'],
                 }]
+            });
+            msg.guild.channels.create(tableShorthand + "ooc", {
+                type: 'text',
+                parent: cat,
             });
         }).catch(console.error);
     } else {
