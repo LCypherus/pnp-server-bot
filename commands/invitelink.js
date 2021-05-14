b920c3a838dc722f7326535d1e342735abd7bb55
LCypherus <km1609@gmail.com>
2021-05-14 09:43:24 +0200

    Updated invitelink

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -8,7 +8,7 @@ module.exports = async function(client /* bot client */, msg /* message object *
 	.setDescription('This is the invite link to invite players to this server.')
 	.setThumbnail('https://cdn.discordapp.com/attachments/834882298268221460/840171923093585940/icon.png')
 	.addFields(
-		{ name: 'Link', value: 'https://discord.gg/ZN4DrdGg' },
+		{ name: 'Link', value: 'https://discord.gg/q8QuBDS' },
 	)
 	.setFooter('&invitelink - Contact L_Cypherus when you\'re having problems with the invite link.');
 
